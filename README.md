74e96fe7e036d65a00c045a59341a5292bd4f599
jcylim <jonathanlcy@gmail.com>
2019-07-22 00:34:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Disaster Response Pipeline Project
+
+## Table of Contents
+
+1. [Installation](#installation)
+* [Dependencies](#depencies)
+* [How to run project](#run_project)
+2. [Project Motivation](#motivation)
+3. [File Descriptions](#files)
+4. [Licensing, Authors, and Acknowledgements](#licensing)
+
+### Installation: <a name="installation"></a>
+#### Dependencies <a name="dependencies"></a>
+In addition to the Anaconda distribution of Python versions 3.5+, please install the following packages: 
+* NumPy, SciPy, Pandas, Sciki-Learn, NLTK
+* Flask, Plotly 
+* SQLAlchemy
+
+#### How to run project <a name="run_project"></a>
+1. Run the following commands in the project's root directory to set up your database and model.
+
+    - To run ETL pipeline that cleans data and stores in database
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - To run ML pipeline that trains classifier and saves
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
+2. Run the following command in the app's directory to run your web app.
+    `python run.py`
+
+3. Go to http://0.0.0.0:3001/
+
+### Project Motivation: <a name="motivation"></a>
+In this project,I have applied data engineering, natural language processing, and machine learing skills to analyze disaster data from Figure Eight to build a machine learning pipeline that contains a model used to classify disaster messages and send the messages to an appropriate disaster relief agency. Also, this project includes a web app where an emergency worker can input a new message and get classification results in several categories.  
+
+### File Description: <a name="files"></a>
+There are a total of three folders in this project as shown below:
+1. **app**
+	* **template**: Folder contains the two HTML files (go.html and master.html) that the Flask web app uses to load content. 
+	* **run.py**: Python code that runs the web app.  
+2. **data**
+    * **disaster_categories.csv**: CSV file that contains data about disaster categories.  
+	* **disaster_messages.csv**: CSV file that contains data about recorded disaster messages.
+	* **DisasterResponse.db**: SQLite database that contains the dataset for purpose of this project.
+    * **ETL Pipeline Preparation.ipynb**: Jupyter notebook used to write and test code for "process_data.py".
+    * **ETL Pipeline Preparation.html**: Exported HTML file from "ETL Pipeline Preparation.ipynb".
+    * **process_data.py**: Python code that extracts, transforms/cleans, and saves processed dataset to SQLite database.
+3. **models**
+	* **classifier.pkl**: Pickle file that contains the machine learning pipeline.
+    * **ML Pipeline Preparation.ipynb**: Jupyter notebook used to write and test code for "train_classifier.py".
+    * **ML Pipeline Preparation.html**: Exported HTML file from "ML Pipeline Preparation.ipynb".
+	* **train_classifier.py**: Python code that builds and trains a machine learning pipeline to output a final model for multi-output classification of disaster messages.  
+
+### Licensing, Authors, Acknowledgements<a name="licensing"></a>
+Must give credit to Figure Eight for the data.  You can find more information about Figure Eight at the link available [here](https://www.figure-eight.com/).  Otherwise, feel free to use the code here as you would like! 
---
+++ models/train_classifier.py
@@ -0,0 +1,175 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import pickle
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+import re
+import numpy as np
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+
+# custom transformer
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    '''
+    Modified StartingVerbExtractor class used to improve analysis performance
+    '''
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+    
+def load_data(database_filepath):
+    '''
+    Load dataset, input set, and labels set from SQLite database.
+    Arguments:
+        database_filepath: path to database where dataset is saved to (String)
+    Returns:
+        X: feature dataset (Pandas Series)
+        y: label dataset (Pandas Series)
+        category_names: list of column names (Pandas Index)
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('df',engine)
+    
+    # load feature set (X), label set (Y), and column names
+    X = df['message'] 
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    
+    return X, y, category_names
+
+def tokenize(text):
+    '''
+    Tokenize text to enable NLP.
+    Arguments:
+        text: English text to be tokenized for ML (List)
+    Returns:
+        clean_tokens: tokenized text for ML (List)
+    '''
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+def build_model():
+    '''
+    Build ML pipeline that includes GridSearch, FeatureUnion, pipeline with CountVectorizer and TfidfTransformer, StartingVerbExtractor, and AdaBoostClassifier for analysis.
+    Returns:
+        model: ML pipeline that contains NLP processes and classifier (Scikit Pipeline)
+    '''
+    # parameters for grid search to improve pipeline performance
+    parameters = {
+        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'features__text_pipeline__vect__max_df': (0.5, 0.75),
+        'features__text_pipeline__vect__max_features': (None, 5000), 
+        'features__text_pipeline__tfidf__use_idf': (True, False)
+    }
+    
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return model
+    
+def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    Evaluate performance of ML pipeline by displaying multiple scores.
+    Arguments:
+        model: ML pipeline to be evaluated (Scikit Pipeline)
+        X_test: test feature dataset (Pandas Series)
+        Y_test: test label dataset (Pandas Series)
+        category_names: list of column names (List)
+    '''
+    # model predictions
+    y_pred = model.predict(X_test)
+    
+    # Overall accuracy of model
+    accuracy = (y_pred == Y_test).mean()
+    print("Overall Accuracy:", accuracy.mean())
+    
+    # scores report
+    y_pred_df = pd.DataFrame(y_pred, columns=category_names)
+    for col in category_names:
+        print('Attribute: {}\n'.format(col))
+        print(classification_report(Y_test[col], y_pred_df[col]))
+
+def save_model(model, model_filepath):
+    '''
+    Build ML pipeline that includes FeatureUnion, pipeline with CountVectorizer and TfidfTransformer, StartingVerbExtractor, and AdaBoostClassifier for analysis.
+    Arguments:
+        model: ML pipeline to be saved (Scikit Pipeline)
+        model_filepath: name of pickle file the model is saved to (String)
+    '''
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
